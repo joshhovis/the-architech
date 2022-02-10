e14e532d756c249b2bca9bfc01024cc18b889a33
joshhovis <joshuahovis3@gmail.com>
2022-02-10 16:05:16 -0500

    Initial commit - React front-end app is up and running with all component folders/files created

---
+++ architech-fe/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import './App.sass';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>The Architech</h1>
+
+    </div>
+  );
+}
+
+export default App;
